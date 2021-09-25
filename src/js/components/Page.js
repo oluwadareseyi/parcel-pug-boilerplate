62756990abcef1193e44e2ed0ed547caad4fd94b
Oluwadare Seyi <oluwadareseyii@gmail.com>
2021-09-25 09:55:26 +0100

    chore: add link listeners and page transitions 👷🏾‍♀️

---
+++ src/js/components/Page.js
@@ -0,0 +1,53 @@
+import AutoBind from "auto-bind";
+
+export default class Page {
+  constructor({ classes, element, elements }) {
+    AutoBind(this);
+
+    this.classes = {
+      ...classes,
+    };
+
+    this.selector = element;
+    this.selectorChildren = { ...elements };
+    this.create();
+  }
+
+  create() {
+    if (this.selector instanceof HTMLElement) {
+      this.element = this.selector;
+    } else {
+      this.element = document.querySelector(this.selector);
+    }
+
+    this.elements = {};
+
+    Object.keys(this.selectorChildren).forEach((key) => {
+      const entry = this.selectorChildren[key];
+
+      if (
+        entry instanceof HTMLElement ||
+        entry instanceof NodeList ||
+        Array.isArray(entry)
+      ) {
+        this.elements[key] = entry;
+      } else {
+        this.elements[key] = this.element.querySelectorAll(entry);
+
+        if (this.elements[key].length === 0) {
+          this.elements[key] = null;
+        } else if (this.elements[key].length === 1) {
+          this.elements[key] = this.element.querySelector(entry);
+        }
+      }
+    });
+  }
+
+  show() {
+    return Promise.resolve();
+  }
+
+  hide() {
+    return Promise.resolve();
+  }
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,96 @@
-class App {}
+import AutoBind from "auto-bind";
+import each from "lodash/each";
+
+import Case from "./pages/Case";
+import Home from "./pages/Home";
+
+class App {
+  constructor() {
+    AutoBind(this);
+
+    this.url = window.location.pathname;
+
+    this.createCase();
+    this.createHome();
+
+    this.pages = {
+      "/": this.home,
+      "/case": this.case,
+    };
+
+    if (this.url.indexOf("/case") > -1) {
+      this.page = this.case;
+      this.page.onResize();
+    } else {
+      this.page = this.pages[this.url];
+    }
+
+    this.page.show(this.url);
+
+    this.addLinksEventsListeners();
+  }
+
+  createHome() {
+    this.home = new Home();
+  }
+
+  createCase() {
+    this.case = new Case();
+  }
+
+  async onChange({ push = true, url = null }) {
+    url = url.replace(window.location.origin, "");
+
+    if (this.isFetching || this.url === url) return;
+
+    this.isFetching = true;
+
+    this.url = url;
+
+    if (this.canvas) {
+      this.canvas.onChange(this.url);
+    }
+
+    await this.page.hide();
+
+    if (push) {
+      window.history.pushState({}, document.title, url);
+    }
+
+    if (this.url.indexOf("/case") > -1) {
+      this.page = this.case;
+    } else {
+      this.page = this.pages[this.url];
+    }
+
+    await this.page.show(this.url);
+
+    this.isFetching = false;
+  }
+
+  addLinksEventsListeners() {
+    const links = document.querySelectorAll("a");
+
+    each(links, (link) => {
+      const isLocal = link.href.indexOf(window.location.origin) > -1;
+
+      if (isLocal) {
+        link.onclick = (event) => {
+          event.preventDefault();
+
+          this.onChange({
+            url: link.href,
+          });
+        };
+      } else if (
+        link.href.indexOf("mailto") === -1 &&
+        link.href.indexOf("tel") === -1
+      ) {
+        link.rel = "noopener";
+        link.target = "_blank";
+      }
+    });
+  }
+}
+
+new App();
